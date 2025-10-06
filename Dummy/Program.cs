cec81f35438d05d64e4d1696bd37dd4af8bef5b1
Giorgos Brantzos <g.brantzos@gmail.com>
2025-10-06 14:28:19 +0300

    OpenTelemetry example

--- Dummy/Program.cs
+++ Dummy/Program.cs
@@ -1,5 +1,8 @@
 using System.Net;
+using Dummy;
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
@@ -16,6 +20,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
 
 app.UseExceptionHandler(options =>
 {
@@ -32,3 +38,29 @@ app.UseExceptionHandler(options =>
     });
 });
 
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", (ITest test) =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
--- Workbench/Program.cs
+++ Workbench/Program.cs
@@ -1,146 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
-IErrorMapperRegistry mapperRegistry = new ErrorMapperRegistry();
-mapperRegistry.Register(new NotFoundErrorMapper());
-mapperRegistry.Register(new InvalidRequestErrorMapper());
+var builder = WebApplication.CreateBuilder(args);
 
-var notFoundMapper = mapperRegistry.GetMapper<NotFound>();
-var notFound = new NotFound();
-var notFoundDetails = notFoundMapper(notFound);
-Console.WriteLine(notFoundDetails);
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService("Sample"))
+    .WithTracing(tracing => tracing
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation()
+        .AddNpgsql())
+    .WithMetrics(metrics => metrics
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation());
 
-var invalidRequestMapper = mapperRegistry.GetMapper<InvalidRequest>();
-var invalidRequest = new InvalidRequest();
-var invalidRequestDetails = invalidRequestMapper(invalidRequest);
-Console.WriteLine(invalidRequestDetails);
-
-Console.ReadKey();
-
-
-public class Error { }
-
-public class ProblemDetails
+builder.Logging.AddOpenTelemetry(options =>
 {
-    public string Type { get; init; } = String.Empty;
-    public long Code { get; init; } = -1;
+    options.IncludeScopes = true;
+    options.IncludeFormattedMessage = true;
+});
 
-    public override string ToString() => $"{Type}, #{Code}";
-}
+builder.Services.AddOpenTelemetry().UseOtlpExporter();
 
-public interface IErrorMapper<in TError> where TError : Error
-{
-    ProblemDetails Map(TError error);
-}
+var app = builder.Build();
 
-public class NotFound : Error { }
-public class InvalidRequest : Error { }
-
-public class NotFoundErrorMapper : IErrorMapper<NotFound>
-{
-    public ProblemDetails Map(NotFound err)
+// the magic 🪄
+var global = app
+    .MapGroup(string.Empty)
+    .AddEndpointFilter<ScreamingFilter>()
+    .AddEndpointFilterFactory((context, next) =>
     {
-        return new ProblemDetails
-        {
-            Type = err.GetType().Name,
-            Code = err.GetHashCode()
-        };
+        var ii = 1;
+        // if ()
+        return next;
+    });
+
+global.MapGet("/hi", () => "Hi");
+global.MapGroup("/what")
+    .MapGet("/now", () => "🤷");
+global.MapGroup("/what").MapGet("/nowagain", () => "🤷");
+
+app.Run();
+
+public class ScreamingFilter : IEndpointFilter
+{
+    public async ValueTask<object?> InvokeAsync(
+        EndpointFilterInvocationContext context,
+        EndpointFilterDelegate next)
+    {
+        var result = await next(context);
+        return result is string s
+            ? $"{s}!!!!"
+            : result;
     }
 }
-
-public class InvalidRequestErrorMapper : IErrorMapper<InvalidRequest>
-{
-    public ProblemDetails Map(InvalidRequest err)
-    {
-        return new ProblemDetails
-        {
-            Type = err.GetType().Name,
-            Code = err.GetHashCode()
-        };
-    }
-}
-
-public interface IErrorMapperRegistry
-{
-    void Register<TError>(IErrorMapper<TError> provider) where TError : Error;
-    Func<TError, ProblemDetails> GetMapper<TError>() where TError : Error;
-}
-
-public class ErrorMapperRegistry : IErrorMapperRegistry
-{
-    private class DelegateHelper<T> where T : Error
-    {
-        public DelegateHelper(IErrorMapper<T> errorMapper)
-        {
-            Func<T, ProblemDetails> mapper = (r) => errorMapper.Map(r);
-            MapperDelegate = error => mapper(error);
-        }
-
-        public Func<T, ProblemDetails> MapperDelegate { get; }
-    }
-
-    private readonly Dictionary<Type, Func<Error, ProblemDetails>> _cache = new(); 
-
-    public void Register<TError>(IErrorMapper<TError> provider) where TError : Error
-    {
-        var delegateHelper = new DelegateHelper<TError>(provider);
-        _cache.Add(typeof(TError), err => delegateHelper.MapperDelegate.Invoke((TError) err));
-    }
-
-    public Func<TError, ProblemDetails> GetMapper<TError>() where TError : Error
-    {
-        var mapper = _cache[typeof(TError)];
-        return error => mapper(error);
-    }
-}
-
-
-// using System;
-// using System.Diagnostics;
-// using System.Threading;
-// using System.Threading.Tasks;
-//
-//
-// var task = Task.Run(() => ConsumeCPU(50));
-//
-// while (true)
-// {
-//     await Task.Delay(2000);
-//     var cpuUsage = await GetCpuUsageForProcess();
-//
-//     Console.WriteLine(cpuUsage);
-// }
-//
-//
-// static void ConsumeCPU(int percentage)
-// {
-//     Stopwatch watch = new Stopwatch();
-//     watch.Start();
-//     while (true)
-//     {
-//         if (watch.ElapsedMilliseconds > percentage)
-//         {
-//             Thread.Sleep(100 - percentage);
-//             watch.Reset();
-//             watch.Start();
-//         }
-//     }
-// }
-//
-// static async Task<double> GetCpuUsageForProcess()
-// {
-//     var startTime = DateTime.UtcNow;
-//     var startCpuUsage = Process.GetCurrentProcess().TotalProcessorTime;
-//
-//     await Task.Delay(1000);
-//
-//     var endTime = DateTime.UtcNow;
-//     var endCpuUsage = Process.GetCurrentProcess().TotalProcessorTime;
-//
-//     var cpuUsedMs = (endCpuUsage - startCpuUsage).TotalMilliseconds;
-//     var totalMsPassed = (endTime - startTime).TotalMilliseconds;
-//
-//     var cpuUsageTotal = cpuUsedMs / (Environment.ProcessorCount * totalMsPassed);
-//
-//     return cpuUsageTotal * 100;
-// }
